699c7680387f39a28e4d9b992845d77a1cbb5105
Mahamadou KOITA <koitamahamadou2000@yahoo.fr>
2016-11-30 00:14:20 -0500

    add sequelize for dataBase

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
+var Sequelize = require('sequelize');
 var express = require('express');
+var databaseURL = 'sqlite://db';
+var sequelize = new Sequelize(databaseURL);	
 var app = express();
 var port = 3000;
 var blogPost = [];
 var bodyParser = require("body-parser");
 
+var Post = sequelize.define('Post', {
+    //create title and content as strings,
+    title: Sequelize.STRING,
+    content: Sequelize.STRING,
+    dateTime:Sequelize.DATE
+});
+
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
 app.get('/list', function(req, res){
 	console.log("hi this lis rendering ");
-	res.render('list', {blogPost: blogPost});
+	Post.findAll().then(function(rows){
+			res.render('list', {blogPost: rows});
+	});
+});
+
+app.get('/Post.json', function(req, res){
+	Post.findAll().then(function(){
+		res.json(Post);
+	});
 });
 
 app.post('/create', function(req, res){
@@ -24,9 +43,14 @@ app.post('/create', function(req, res){
 	blogPost.push(newEntre);
 
 	console.log('Hi guys this my express app');
-	res.render('list', {blogPost: blogPost});
+	Post.create(newEntre).then(function(){
+		res.render('list', {blogPost: blogPost});
+	});
+	
  });
+sequelize.sync().then(function(){
+	app.listen(port, function(){
+		console.log("app on port "+ port);
+	});
 
-app.listen(port, function(){
-	console.log("app on port "+ port);
 });
