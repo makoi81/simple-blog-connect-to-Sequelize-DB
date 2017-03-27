d742ff3a65629d66c294bc3fe672b6caebe63aaf
Mahamadou KOITA <koitamahamadou2000@yahoo.fr>
2017-03-26 21:04:49 -0400

    cleaning the code

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ var databaseURL = 'sqlite://db';
 var sequelize = new Sequelize(databaseURL);	
 var app = express();
 var port = 3000;
-var blogPost = [];
 var bodyParser = require("body-parser");
 
 var Post = sequelize.define('Post', {
-    //create title and content as strings,
     title: Sequelize.STRING,
     content: Sequelize.STRING,
     dateTime:Sequelize.DATE
@@ -20,7 +18,7 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
-app.get('/list', function(req, res){
+app.get('/', function(req, res){
 	console.log("hi this lis rendering ");
 	Post.findAll().then(function(rows){
 			res.render('list', {blogPost: rows});
@@ -39,15 +37,14 @@ app.post('/create', function(req, res){
 		'title': req.body.title,
 		'content':req.body.content,
 		'dateTime': d
-	}
-	blogPost.push(newEntre);
-
+	}	
 	console.log('Hi guys this my express app');
 	Post.create(newEntre).then(function(){
 		res.render('list', {blogPost: blogPost});
 	});
 	
- });
+});
+
 sequelize.sync().then(function(){
 	app.listen(port, function(){
 		console.log("app on port "+ port);
